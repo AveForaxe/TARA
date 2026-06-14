62d606984bb48c4505da54be5cabf56d2f26d50a
farell <revokaeng4@gmail.com>
2026-06-14 14:29:45 +0700

    first commit

--- backend/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/handshake", authController.handshakeQR);
-
-module.exports = router;
